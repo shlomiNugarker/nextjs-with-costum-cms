34001fe9c93f84f3d2635adbb2c576b12c366a9e
Shlomi Nugarker <shlomin1231@gmail.com>
2024-11-01 04:14:58 +0200

    getNurseryProducts

--- src/app/nursery/page.tsx
+++ src/app/nursery/page.tsx
@@ -1,93 +1,9 @@
 import React from "react";
 import { NurseryCard } from "@/cmps/NurseryCard";
+import { getNurseryProducts } from "@/lib/queries";
 
-const nurseryProducts = [
-  {
-    id: 1,
-    name: "רוזמרין",
-    description: "צמח תבלין ארומטי, אידיאלי לתיבול מנות בשר, מרקים וסלטים.",
-    potSize: "15 ס״מ",
-    category: "תבלינים",
-    image:
-      "https://images.unsplash.com/photo-1523738914649-b0d2753887a1?q=80&w=1030&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    price: "₪15",
-  },
-  {
-    id: 2,
-    name: "נענע",
-    description: "צמח רב שנתי בעל עלים רעננים, נהדר לתה, משקאות ותבשילים.",
-    potSize: "15 ס״מ",
-    category: "תבלינים",
-    image:
-      "https://images.unsplash.com/photo-1708481480624-f27f9c1cc891?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    price: "₪12",
-  },
-  {
-    id: 3,
-    name: "בזיליקום מתוק",
-    description: "צמח תבלין עם עלים גדולים וריחניים, מתאים לפסטה, סלטים ועוד.",
-    potSize: "15 ס״מ",
-    category: "תבלינים",
-    image:
-      "https://images.unsplash.com/photo-1515542647469-5f9a6b25ef5b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    price: "₪18",
-  },
-  {
-    id: 4,
-    name: "עץ לימון",
-    description: "עץ פרי קטן לגידול בגינה או בעציץ, מניב פירות טריים כל השנה.",
-    potSize: "25 ס״מ",
-    category: "עצים",
-    image:
-      "https://images.unsplash.com/photo-1605185189315-fc269c231e41?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    price: "₪75",
-  },
-  {
-    id: 5,
-    name: "עץ זית",
-    description:
-      "עץ חזק ועמיד, מתאים לגידול בגינה או בעציץ גדול, מניב זיתים איכותיים.",
-    potSize: "30 ס״מ",
-    category: "עצים",
-    image:
-      "https://images.unsplash.com/photo-1541259418332-97b56947904f?q=80&w=989&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    price: "₪120",
-  },
-  {
-    id: 6,
-    name: "לבנדר",
-    description:
-      "צמח נוי ותבלין עם פריחה סגולה מרהיבה, מתאים להרחיק מזיקים ולהשרות ריח נעים.",
-    potSize: "15 ס״מ",
-    category: "נוי",
-    image:
-      "https://images.unsplash.com/photo-1531112606622-e8174567b048?q=80&w=994&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    price: "₪20",
-  },
-  {
-    id: 7,
-    name: "מרווה",
-    description:
-      "צמח תבלין רב שנתי עם עלים ריחניים, מצוין לתיבול ולרפואה טבעית.",
-    potSize: "15 ס״מ",
-    category: "תבלינים",
-    image:
-      "https://images.unsplash.com/photo-1632346265081-eb7e5c507721?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    price: "₪15",
-  },
-  {
-    id: 8,
-    name: "פטרוזיליה",
-    description: "צמח תבלין עשיר בויטמינים, מושלם לסלטים ולתבשילים.",
-    potSize: "15 ס״מ",
-    category: "תבלינים",
-    image:
-      "https://images.unsplash.com/photo-1528796940112-4979b4a98424?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    price: "₪12",
-  },
-];
-
-export default function Page() {
+export default async function Page() {
+  const nurseryProducts = await getNurseryProducts();
   return (
     <>
       <div className="pb-12 pt-24 px-4 max-w-screen-lg mx-auto mt-2 min-h-screen">
