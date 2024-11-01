a3bbb20c77e96f2205a004c025fcfacfea7d89e7
Shlomi Nugarker <shlomin1231@gmail.com>
2024-11-01 12:51:18 +0200

    users

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -19,3 +19,12 @@ export const nurseryProductsTable = pgTable("nursery_products", {
   price: integer("price").notNull(),
   image_url: text("image_url"),
 });
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  username: varchar("username", { length: 64 }).notNull(),
+  email: varchar("email", { length: 64 }).notNull().unique(),
+  password: varchar("password", { length: 64 }).notNull(),
+  profile_image_url: text("profile_image_url"),
+  created_at: varchar("created_at", { length: 255 }).default("now()"),
+});
