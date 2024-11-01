c1ab9f3c7330d54d2887be916b2a71462a8fc384
Shlomi Nugarker <shlomin1231@gmail.com>
2024-11-01 16:42:43 +0200

    style

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -21,7 +21,7 @@ export default function Login() {
   }
 
   return (
-    <div className="flex items-center justify-center bg-theme-light dark:bg-darkmode-theme-light">
+    <div className="flex items-center justify-center bg-theme-light dark:bg-darkmode-theme-light min-h-screen">
       <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border  shadow-xl">
         <div className="flex flex-col items-center justify-center space-y-3 border-b px-4 py-6 pt-8 text-center sm:px-16">
           <h3 className="text-xl font-semibold">Sign Up</h3>
