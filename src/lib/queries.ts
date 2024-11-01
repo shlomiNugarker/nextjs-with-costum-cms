cad5000f89e2b7cd57cd19b8af5249be6a7a3217
Shlomi Nugarker <shlomin1231@gmail.com>
2024-11-01 04:15:10 +0200

    getNurseryProducts

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,6 +1,6 @@
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
-import { weeklyProductsTable } from "./schema";
+import { nurseryProductsTable, weeklyProductsTable } from "./schema";
 
 const client = postgres(process.env.POSTGRES_URL || "");
 const db = drizzle(client);
@@ -11,6 +11,16 @@ export async function getWeeklyProducts() {
     return products;
   } catch (error) {
     console.error("Error fetching weekly products:", error);
+    return [];
+  }
+}
+
+export async function getNurseryProducts() {
+  try {
+    const products = await db.select().from(nurseryProductsTable);
+    return products;
+  } catch (error) {
+    console.error("Error fetching nursery products:", error);
     return [];
   }
 }
--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -9,3 +9,13 @@ export const weeklyProductsTable = pgTable("weekly_products", {
   price: integer("price").notNull(),
   image_url: text("image_url"),
 });
+
+export const nurseryProductsTable = pgTable("nursery_products", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 100 }).notNull(),
+  description: text("description"),
+  pot_size: varchar("pot_size", { length: 50 }),
+  category: varchar("category", { length: 50 }),
+  price: integer("price").notNull(),
+  image_url: text("image_url"),
+});
