b2af120cdc514296bd292039992b9188cfb7a1d7
Shlomi Nugarker <shlomin1231@gmail.com>
2024-11-13 09:28:04 +0200

    api to upload image to clodinary

---
+++ src/app/api/upload/cloudinary/route.ts
@@ -0,0 +1,55 @@
+import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
+import { Readable } from "stream";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export async function POST(request: NextRequest) {
+  try {
+    const formData = await request.formData();
+    const file = formData.get("file");
+
+    if (!file || !(file instanceof Blob)) {
+      return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
+    }
+
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const stream = Readable.from(buffer);
+
+    const uploadResult = await new Promise<UploadApiResponse>(
+      (resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream(
+          { folder: "hagina_bapardes" },
+          (error, result) => {
+            if (error || !result) {
+              reject(new Error("Upload failed"));
+            } else {
+              resolve(result);
+            }
+          }
+        );
+        stream.pipe(uploadStream);
+      }
+    );
+
+    return NextResponse.json({
+      url: uploadResult.secure_url,
+    });
+  } catch (error) {
+    console.error("Error uploading the image:", error);
+    return NextResponse.json(
+      { error: "Failed to upload image" },
+      { status: 500 }
+    );
+  }
+}
