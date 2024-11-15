ba151bc516492a4c112d4778148821712069459d
Shlomi Nugarker <shlomin1231@gmail.com>
2024-11-15 18:54:51 +0200

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,36 @@ import { HeroImg } from "@/cmps/HeroImg";
 import { getContentBlocksByPageId } from "@/services/db/repositories/contentBlockRepository";
 import { getPageByName } from "@/services/db/repositories/pageRepository";
 
+interface Page {
+  id: number;
+  name: string;
+  title: string | null;
+  meta_title: string | null;
+  meta_description: string | null;
+  meta_keywords: string | null;
+  description: string | null;
+  created_at: Date | null;
+}
+
+interface ContentBlock {
+  id: number;
+  created_at: Date | null;
+  page_id: number;
+  block_type: string;
+  content: string;
+  position: number | null;
+}
+
 export default async function Home() {
-  const homePage = await getPageByName("home");
+  const homePage: Page | null = await getPageByName("home");
 
   if (!homePage) {
     return <div>דף הבית לא נמצא</div>;
   }
 
-  const contentBlocks = await getContentBlocksByPageId(homePage.id);
+  const contentBlocks: ContentBlock[] = await getContentBlocksByPageId(
+    homePage.id
+  );
 
   const heroUrl = contentBlocks.find(
     (block) => block.block_type === "text"
@@ -21,17 +43,17 @@ export default async function Home() {
   )?.content;
 
   return (
-    <main className="">
+    <main>
       <section className="mt-5 h-[100vh] w-full flex flex-col md:flex-row justify-center items-center text-customNavy px-4 animate-float">
         <div className="max-w-[500px] text-center md:text-right animate-fade-in-up">
-          <h1 className="text-4xl md:text-[70px] font-bold leading-tight ">
+          <h1 className="text-4xl md:text-[70px] font-bold leading-tight">
             {homePage.title}
           </h1>
           <br />
           <p className="text-2xl">{homePage.description}</p>
         </div>
         <div className="mt-8 md:mt-0 md:ml-8 flex justify-center animate-fade-in">
-          <HeroImg href={heroUrl} />
+          <HeroImg href={heroUrl || ""} />
         </div>
       </section>
 
