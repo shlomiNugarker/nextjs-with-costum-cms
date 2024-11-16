a9078ed69934821131d29bdd02cac6bae6674b86
Shlomi Nugarker <shlomin1231@gmail.com>
2024-11-16 21:17:44 +0200

    גכ

--- src/cmps/ProductCard.tsx
+++ src/cmps/ProductCard.tsx
@@ -13,29 +13,33 @@ interface Product {
 
 export const ProductCard = ({ product }: { product: Product }) => {
   return (
-    <div className="border rounded-lg overflow-hidden shadow-md bg-white flex flex-col justify-between">
+    <div className="border rounded-lg overflow-hidden shadow-lg bg-white flex flex-col justify-between transition-transform duration-300 transform hover:scale-105 hover:shadow-2xl">
       <div>
         <div>
           <Image
             src={product.image_url || ""}
             alt={product.name}
-            className="w-full h-48 object-cover"
-            width={300}
-            height={150}
+            className="w-full h-60 object-cover rounded-t-lg"
+            width={400}
+            height={200}
           />
         </div>
-        <div className="p-4 flex flex-col justify-between">
+        <div className="p-6 flex flex-col justify-between">
           <div>
-            <h2 className="text-xl font-semibold text-customNavy mb-2">
+            <h2 className="text-2xl font-bold text-customNavy mb-3">
               {product.name}
             </h2>
-            <p className="text-gray-600 mb-4">{product.description}</p>
-            <p className="text-gray-500 mb-2">משקל: {product.weight}</p>
+            <p className="text-gray-700 mb-4 leading-relaxed">
+              {product.description}
+            </p>
+            <p className="text-gray-500 mb-3 font-medium">
+              משקל: {product.weight}
+            </p>
           </div>
         </div>
       </div>
-      <div>
-        <p className="text-customGreen font-bold text-2xl text-center">
+      <div className="bg-customPeach py-4">
+        <p className="text-customGreen font-bold text-3xl text-center">
           {product.price} ש&quot;ח
         </p>
       </div>
