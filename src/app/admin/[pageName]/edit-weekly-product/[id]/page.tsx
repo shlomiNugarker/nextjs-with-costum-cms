cee0c54ca83fed7cdc7ccba5d9077d6767996a0c
Shlomi Nugarker <shlomin1231@gmail.com>
2024-11-18 20:15:47 +0200

    AdminEditPage

--- src/app/admin/[pageName]/edit-weekly-product/[id]/page.tsx
+++ src/app/admin/[pageName]/edit-weekly-product/[id]/page.tsx
@@ -1,25 +1,41 @@
 import { WeeklyProductsForm } from "@/cmps/admin/WeeklyProductsForm";
-import { getProductById } from "@/services/db/repositories/productRepository";
+import { genericRepository } from "@/services/db/repositories/genericRepository";
 
 export const revalidate = 5;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default async function EditWeeklyProduct(props: any) {
-  const productId = parseInt(props.params.id, 10);
-  const product = await getProductById(productId, "weekly");
+  try {
+    const productId = parseInt(props.params.id, 10);
+    if (isNaN(productId)) {
+      throw new Error("Invalid product ID");
+    }
 
-  return (
-    <>
-      <section className="min-h-screen flex flex-col items-center justify-center space-y-6 bg-gray-100 text-customNavy">
-        <h2 className="text-4xl font-bold text-center mb-6 text-customNavy">
-          ערוך מוצר בתוצרת השבועית
-        </h2>
-        {product ? (
-          <WeeklyProductsForm initialProduct={product} />
-        ) : (
-          <p>לא נמצא מוצר עם מזהה זה</p>
-        )}
+    const product = await genericRepository.getById(
+      "weeklyProductsTable",
+      productId
+    );
+
+    return (
+      <>
+        <section className="min-h-screen flex flex-col items-center justify-center space-y-6 bg-gray-100 text-customNavy">
+          <h2 className="text-4xl font-bold text-center mb-6 text-customNavy">
+            ערוך מוצר בתוצרת השבועית
+          </h2>
+          {product ? (
+            <WeeklyProductsForm initialProduct={product} />
+          ) : (
+            <p>לא נמצא מוצר עם מזהה זה</p>
+          )}
+        </section>
+      </>
+    );
+  } catch (error) {
+    console.error("Error fetching weekly product:", error);
+    return (
+      <section className="min-h-screen flex items-center justify-center bg-gray-100 text-customNavy">
+        שגיאה בטעינת המידע של המוצר
       </section>
-    </>
-  );
+    );
+  }
 }
--- src/app/admin/[pageName]/page.tsx
+++ src/app/admin/[pageName]/page.tsx
@@ -1,36 +1,54 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { AdminNurseryList } from "@/cmps/admin/AdminNurseryList";
 import { AdminPostsList } from "@/cmps/admin/AdminPostsList";
 import { AdminWeeklyList } from "@/cmps/admin/AdminWeeklyList";
 import { ContentBlockEditForm } from "@/cmps/admin/ContentBlockEditForm";
 import { PageEditForm } from "@/cmps/admin/PageEditForm";
-import { getContentBlocksByPageId } from "@/services/db/repositories/contentBlockRepository";
-import { getPageByName } from "@/services/db/repositories/pageRepository";
+import { genericRepository } from "@/services/db/repositories/genericRepository";
 
 export const revalidate = 5;
+
 interface Params {
   params: {
     pageName: string;
   };
 }
+
 export default async function AdminEditPage({ params }: Params) {
-  const { pageName } = params;
-  const page = await getPageByName(pageName);
-  if (!page) {
-    return <div> לא נמצא דף כזה</div>;
+  try {
+    const { pageName } = params;
+
+    const page: any = await genericRepository.getByField(
+      "pagesTable",
+      "name",
+      pageName
+    );
+    if (!page) {
+      return <div>לא נמצא דף כזה</div>;
+    }
+
+    const contentBlocks: any = await genericRepository.getAllWithFilter(
+      "contentBlocksTable",
+      {
+        page_id: page.id,
+      }
+    );
+
+    return (
+      <div className="pb-12 px-4 max-w-screen-lg mx-auto min-h-[calc(100vh-70px)] justify-center items-center flex flex-col pt-5 text-customNavy">
+        <h1 className="text-3xl font-semibold text-center mb-6">
+          ערוך עמוד {pageName}
+        </h1>
+        <PageEditForm initialPage={page} />
+        <ContentBlockEditForm contentBlocks={contentBlocks} />
+
+        {pageName === "blog" ? <AdminPostsList /> : null}
+        {pageName === "nursery" ? <AdminNurseryList /> : null}
+        {pageName === "weekly-produce" ? <AdminWeeklyList /> : null}
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching page or content blocks:", error);
+    return <div>שגיאה בטעינת המידע של הדף</div>;
   }
-  const contentBlocks = await getContentBlocksByPageId(page.id);
-
-  return (
-    <div className="pb-12 px-4 max-w-screen-lg mx-auto  min-h-[calc(100vh-70px)] justify-center items-center flex flex-col pt-5 text-customNavy">
-      <h1 className="text-3xl font-semibold text-center mb-6">
-        ערוך עמוד {pageName}
-      </h1>
-      <PageEditForm initialPage={page} />
-      <ContentBlockEditForm contentBlocks={contentBlocks} />
-
-      {pageName === "blog" ? <AdminPostsList /> : null}
-      {pageName === "nursery" ? <AdminNurseryList /> : null}
-      {pageName === "weekly-produce" ? <AdminWeeklyList /> : null}
-    </div>
-  );
 }
