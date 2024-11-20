f5cb824490ad70f84ebdb747f56f6b73241bd1be
Shlomi Nugarker <shlomin1231@gmail.com>
2024-11-20 04:05:06 +0200

    use nextauth as middlware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,35 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 import NextAuth from "next-auth";
 import { authConfig } from "./config/auth.config";
 
-const { auth } = NextAuth(authConfig);
+// const { auth } = NextAuth(authConfig);
 
-export default async function middleware(request: NextRequest) {
-  try {
-    const session = await auth();
+export default NextAuth(authConfig).auth;
 
-    if (!session || !session.user) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
+// export default async function middleware(request: NextRequest) {
+//   try {
+//     console.log("middleware");
 
-    const user = session.user;
-    const isAdminRoute = request.nextUrl.pathname.startsWith("/admin");
+//     const session = await auth();
 
-    if (isAdminRoute && user.role !== "Admin") {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
+//     if (!session || !session.user) {
+//       return NextResponse.redirect(new URL("/", request.url));
+//     }
 
-    return NextResponse.next();
-  } catch (error) {
-    console.error("Middleware auth error:", error);
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-}
+//     const user = session.user;
+//     const isAdminRoute = request.nextUrl.pathname.startsWith("/admin");
+
+//     if (isAdminRoute && user.role !== "Admin") {
+//       return NextResponse.redirect(new URL("/", request.url));
+//     }
+
+//     return NextResponse.next();
+//   } catch (error) {
+//     console.error("Middleware auth error:", error);
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
+// }
 
 export const config = {
   matcher: ["/api/:path*", "/admin/:path*", "/admin"],
