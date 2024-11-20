c7420849a2d1035ce301c2d3ec02c93d0582ca22
Shlomi Nugarker <shlomin1231@gmail.com>
2024-11-20 02:01:44 +0200

    add types in uploadImageToCloudinary

--- src/services/client-api/clodinaryApi.ts
+++ src/services/client-api/clodinaryApi.ts
@@ -1,32 +1,35 @@
 import httpService from "../httpService";
 
-export async function uploadImageToCloudinary(file: File): Promise<string> {
+export async function uploadImageToCloudinary(file: File) {
   const formData = new FormData();
   formData.append("file", file);
 
   try {
-    const response = await httpService.post("/cloudinary/upload", formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return (response.data as any).url;
+    const response: { data: { url: string } } = await httpService.post(
+      "/cloudinary/upload",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return response.data.url;
   } catch (error) {
     console.error("Error uploading image:", error);
     throw new Error("Failed to upload image");
   }
 }
 
-export async function getAllImagesFromCloudinary(
-  folder: string
-): Promise<{ url: string; public_id: string }[]> {
+export async function getAllImagesFromCloudinary(folder: string) {
   try {
-    const response = await httpService.get(`/cloudinary`, {
-      params: { folder },
-    });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return (response.data as any).images;
+    const response: { data: { images: string[] } } = await httpService.get(
+      `/cloudinary`,
+      {
+        params: { folder },
+      }
+    );
+    return response.data.images;
   } catch (error) {
     console.error("Error fetching images:", error);
     throw new Error("Failed to fetch images");
