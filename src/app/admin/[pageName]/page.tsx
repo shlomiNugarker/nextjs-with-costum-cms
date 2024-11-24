219395b436219feee9063ae6145e258cf3561775
Shlomi Nugarker <shlomin1231@gmail.com>
2024-11-25 00:27:48 +0200

    remove folders, use generic form

--- src/app/admin/[pageName]/page.tsx
+++ src/app/admin/[pageName]/page.tsx
@@ -2,7 +2,7 @@ import { AdminNurseryList } from "@/cmps/admin/AdminNurseryList";
 import { AdminPostsList } from "@/cmps/admin/AdminPostsList";
 import { AdminWeeklyList } from "@/cmps/admin/AdminWeeklyList";
 import { ContentBlockEditForm } from "@/cmps/admin/ContentBlockEditForm";
-import { PageEditForm } from "@/cmps/admin/PageEditForm";
+import { GenericEditForm } from "@/cmps/admin/GenericEditForm";
 import { genericRepository } from "@/services/db/repositories/genericRepository";
 
 export const revalidate = 5;
@@ -29,12 +29,25 @@ export default async function AdminEditPage({ params }: Params) {
         })
       : null;
 
+    if (!page) {
+      return <div>לא נמצא רשומה</div>;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { created_at, ...rest } = page;
+    const fields = Object.keys(rest);
+
     return (
       <div className="pb-12 px-4 max-w-screen-lg mx-auto min-h-[calc(100vh-70px)] justify-center items-center flex flex-col pt-5 text-customNavy">
         <h1 className="text-3xl font-semibold text-center mb-6">
           ערוך עמוד {pageName}
         </h1>
-        <PageEditForm initialPage={page} />
+        <GenericEditForm
+          fields={fields}
+          tableName={"pagesTable"}
+          record={rest}
+        />
+        {/* <PageEditForm initialPage={page} /> */}
         {page?.id && (
           <ContentBlockEditForm contentBlocks={contentBlocks || []} />
         )}
---
+++ src/app/admin/add/[tableName/[id]/page.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { GenericEditForm } from "@/cmps/admin/GenericEditForm";
+import { TableName } from "@/services/db/schema";
+
+type PageProps<T extends TableName> = {
+  params: {
+    tableName: T;
+    id: string;
+  };
+};
+
+export const revalidate = 5;
+
+export default async function Page<T extends TableName>({
+  params,
+}: PageProps<T>) {
+  const record = { id: 1, name: "test" }; // get empty record ???
+
+  if (!record) {
+    return <div>לא נמצא רשומה עם מזהה זה</div>;
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { created_at, ...rest } = record as any;
+  const fields = Object.keys(rest);
+
+  return (
+    <div className="pb-12 px-4 max-w-screen-lg mx-auto min-h-[calc(100vh-70px)] justify-center items-center flex flex-col pt-5 text-customNavy">
+      <GenericEditForm
+        fields={fields}
+        tableName={params.tableName}
+        record={rest}
+      />
+    </div>
+  );
+}
