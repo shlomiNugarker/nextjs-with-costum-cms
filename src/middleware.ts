cae86499f89f319521fe3b3b0d20bd0a5aa24baa
Shlomi Nugarker <shlomin1231@gmail.com>
2024-11-28 11:49:05 +0200

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,12 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./config/auth.config";
 
+
 export default NextAuth(authConfig).auth;
 
+
+
 export const config = {
-  matcher: ["/api/:path*", "/admin/:path*", "/admin"],
+  matcher: ["/admin/:path*", "/admin"],
+  // matcher: ["/api/:path*", "/admin/:path*", "/admin"],
 };
